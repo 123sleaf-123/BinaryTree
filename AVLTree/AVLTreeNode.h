6bc315c56ed5e12d8c3eb89f8433fcedbf0a44d5
sleaf <huangzhiye2002@163.com>
2022-03-04 15:53:28 +0800

    Ahahahaha, my tasks, having finished!
    I fix and test the Insert operation, it works well. And it seems that the attribute 'height' is nonsense.
    Check it can work or not.
    Nothing special.

--- AVLTree/AVLTreeNode.h
+++ AVLTree/AVLTreeNode.h
@@ -15,10 +15,12 @@ struct AVLNode {
     ElementType data;
     AVLTree left;
     AVLTree right;
-    int height;
+//    int height;
 };
 
 int max(int a, int b);
+
+int GetHeight(AVLTree T);
 
 AVLTree LeftRotation(AVLTree AVLT);
 
@@ -30,16 +32,29 @@ AVLTree RightLeftRotation(AVLTree AVLT);
 
 AVLTree Insert(ElementType val, AVLTree AVLT);
 
+AVLTree Delete(ElementType val, AVLTree AVLT);
+
 int max(int a, int b) {
     return a > b ? a : b;
+}
+
+int GetHeight(AVLTree T) {
+    if (!T) return -1;
+    else {
+        int a = GetHeight(T->left);
+        int b = GetHeight(T->right);
+//        T->height = (a > b ? a : b) + 1;
+//        return T->height;
+        return (a > b ? a : b) + 1;
+    }
 }
 
 AVLTree LeftRotation(AVLTree AVLT) {
     AVLTree temp = AVLT->right;
     AVLT->right = NULL;
     temp->left = AVLT;
-    AVLT->height = AVLT->left->height + 1;
-    temp->height = max(temp->left->height, temp->right->height) + 1;
+    AVLT->height = GetHeight(AVLT->left) + 1;
+    temp->height = max(GetHeight(temp->left), GetHeight(temp->right)) + 1;
     return temp;
 }
 
@@ -47,8 +62,8 @@ AVLTree RightRotation(AVLTree AVLT) {
     AVLTree temp = AVLT->left;
     AVLT->left = NULL;
     temp->right = AVLT;
-    AVLT->height = AVLT->right->height + 1;
-    temp->height = max(temp->left->height, temp->right->height) + 1;
+    AVLT->height = GetHeight(AVLT->right) + 1;
+    temp->height = max(GetHeight(temp->left), GetHeight(temp->right)) + 1;
     return temp;
 }
 
@@ -71,26 +86,32 @@ AVLTree Insert(ElementType val, AVLTree AVLT) {
     }
 
     // Insert val to the left son trees
-    if (val < AVLT->data) {
+    else if (val < AVLT->data) {
         AVLT->left = Insert(val, AVLT->left);
 
         // Judge whether the tree should rotate or not
-        if (abs(AVLT->left->height - AVLT->right->height) > 1) {
+        if (abs(GetHeight(AVLT->left) - GetHeight(AVLT->right)) > 1) {
             if(val < AVLT->left->data) AVLT = RightRotation(AVLT);
             else AVLT = LeftRightRotation(AVLT);
         }
     }
 
     // Insert val to the right son tree
-    if (val > AVLT->data) {
+    else if (val > AVLT->data) {
         AVLT->right = Insert(val, AVLT->right);
 
         // Judge whether the tree should rotate or not
-        if(abs(AVLT->left->height - AVLT->right->height) > 1) {
+        if(abs(GetHeight(AVLT->left) - GetHeight(AVLT->right)) > 1) {
             if (val > AVLT->right->data) AVLT = LeftRotation(AVLT);
             else AVLT = RightLeftRotation(AVLT);
         }
     }
+
+    return AVLT;
 }
+
+//AVLTree Delete(ElementType val, AVLTree AVLT) {
+//    if (val != AVLT->data)
+//}
 
 #endif //TREE_AVLTREENODE_H
--- main.c
+++ main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
-#include "BinaryTree/BTopts.h"
+//#include "BinaryTree/BTopts.h"
+#include "AVLTree/AVLTreeNode.h"
 
 int main() {
     printf("Hello, World!\n");
-    Stack S = CreateStack();
+    int i = 0;
+    switch (i) {
+        case 0: {
+            AVLTree T;
+            for (int i = 0; i < 10; ++i) {
+                T = Insert((9 - i), T);
+            }
+            break;
+        }
+        case 1: break;
+    }
     return 0;
 }
